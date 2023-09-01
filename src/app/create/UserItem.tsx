bc70d21512bbf858388c105f9b37c140303bc4e6
KR Shanto <krshanto2005@gmail.com>
2023-09-01 11:16:20 +0600

    Create `UserItem` component to display users after search

---
+++ src/app/create/UserItem.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { UserType } from "../../components/UserInput";
+import UserImage from "@/../public/user-image.png";
+import Image from "next/image";
+import { FaTimes } from "react-icons/fa";
+import { cn } from "@/lib/cn";
+
+export default function UserItem({
+  user,
+  className,
+  children,
+}: {
+  user: UserType;
+  className?: string;
+  children?: React.ReactNode;
+}) {
+  return (
+    <div
+      key={user._id}
+      className={cn(
+        "flex items-center justify-between  rounded-md bg-slate-100 px-5 py-2 shadow-lg dark:bg-slate-800",
+        className,
+      )}
+    >
+      <div className="flex items-center space-x-3">
+        <Image
+          src={user.image ? user.image : UserImage}
+          alt={user.name}
+          width={40}
+          height={40}
+          className="h-12 w-12 rounded-full"
+        />
+
+        <div>
+          <p className="mb-2 text-xl font-bold">{user.name}</p>
+          <span
+            className={`rounded-lg border px-3 py-[1px] text-sm ${
+              user.isFriend
+                ? "border-green-500 text-green-500"
+                : "border-red-500 text-red-500"
+            }`}
+          >
+            {user.isFriend ? "Friend" : "Not friend"}
+          </span>
+        </div>
+      </div>
+
+      {children}
+    </div>
+  );
+}
