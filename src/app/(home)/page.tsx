a96684b9a73ed381867b3f1bd719df24602fb205
KR Shanto <krshanto2005@gmail.com>
2024-07-20 06:58:04 +0600

    fix: money fetching opposite user name

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { dbConnect } from "@/lib/dbConnect";
 import DisplayMoney from "./DisplayMoney";
 import { getAuthSession } from "@/lib/auth";
 import LandingPage from "./LandingPage";
+import { User } from "@/models/user";
 
 export default async function Page() {
   const session = (await getAuthSession()) as {
@@ -15,6 +16,8 @@ export default async function Page() {
     return <LandingPage />;
   }
 
+  const id = session.user.id;
+
   await dbConnect();
 
   const money = await Money.find({
@@ -23,6 +26,7 @@ export default async function Page() {
     //   id: session.user.id,
     // },
     //
+    // TODO
     // $or: [
     //   { userId: session.user.id },
     //   {
@@ -36,9 +40,26 @@ export default async function Page() {
 
   const moneyPurified = money.map((money) => money.purify());
 
+  const newMoney = await Promise.all(
+    moneyPurified.map(async (money) => {
+      if (money.oppositeUser.id == id) {
+        const user = await User.findById(money.createdBy);
+        return {
+          ...money,
+          oppositeUser: {
+            ...money.oppositeUser,
+            name: user?.name,
+          },
+        };
+      } else {
+        return money;
+      }
+    }),
+  );
+
   return (
     <>
-      <DisplayMoney money={moneyPurified} />
+      <DisplayMoney money={newMoney} />
     </>
   );
 }
